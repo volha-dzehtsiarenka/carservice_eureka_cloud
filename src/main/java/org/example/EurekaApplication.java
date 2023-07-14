eb48ffe578749eec09b5ed169c5b7761f000d9ef
Volha Dzehtsiarenka <volha_dzehtsiarenka@senla.eu>
2023-07-14 17:40:53 +0300

    Initial commit

---
+++ src/main/java/org/example/EurekaApplication.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Описание класса
+ *
+ * @author Volha Dzehtsiarenka
+ * @since ${DATE}
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
